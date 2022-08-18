193b72be249397bebc2bb2332936a5bcfaf1cef8
deedyago <deedyagobin@gmail.com>
2022-08-18 22:30:11 +0300

    lesson 5 finished + completed tasks

---
+++ 1_lessons/lesson-5/task-2/Program.cs
@@ -0,0 +1,30 @@
+﻿
+int[] array = new int[10];
+int[] array2 = new int[(array.Length/2)];
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(1,15);
+}
+
+void Find(int[] arr)
+{
+    int sumtemp = 0;    
+    for(int i = 0; i < arr.Length/2; i++)
+    {
+        sumtemp = arr[i] + arr[arr.Length - 1 -i];
+        array2[i] = sumtemp;
+    }
+}
+Find(array);
+
+
+void Print(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+       Console.Write($"{arr[i]} ");
+    }
+}
+Print(array);
+Console.WriteLine();
+Print(array2);
---
+++ 1_lessons/lesson-5/task_1/Program.cs
@@ -0,0 +1,28 @@
+﻿int[] array = new int[10];
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(1,134);
+}
+
+int Find2(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] >= 10 && arr[i] <= 99)
+        {
+            count += 1;
+        }
+    }
+    return count;
+}
+void Print(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+       Console.Write($"{arr[i]} ");
+    }
+}
+Print(array);
+Console.WriteLine($" = {Find2(array)}");
