717be0cacbdee579024d9aa2da400b85924fe202
deedyago <deedyagobin@gmail.com>
2022-08-26 03:53:22 +0300

    Частотный анализ task full DONE 60 task

---
+++ 3_Esli-sdelayu-stanu-Senior'om-100proc/6_TwoDimensional-Arrays/task-60+/task-60+.cs
@@ -0,0 +1,79 @@
+﻿// 60. Частотные словарь для ввода пользователя
+using _0_Library;
+using System.Collections;
+
+// метод для рандомного массива двумерного массива
+double[] PowerOfString (char[] array, string ascii)
+{
+    double[] temparray = new double[ascii.Length];
+    int CountSymbols = 0;
+    int CountNumbers = 0;
+    int CountLettersEng = 0;
+    int CountLettersRu = 0;
+    int tempCount = 0;
+    int trueOrFalse = 0;
+
+    for (int i = 0; i < ascii.Length; i++)
+    {
+        for (int j = 0; j < array.Length; j++)
+        {
+            if (array[j] == ascii[i])
+            {
+                        tempCount++;
+                        trueOrFalse = Convert.ToInt32(array[j]);
+                        if(trueOrFalse >= 48 && trueOrFalse <= 57)
+                        {
+                                temparray[i] = temparray[i] + 1.0;
+                                CountNumbers++;
+                                
+                        }
+                        if(trueOrFalse >= 65 && trueOrFalse <= 122)
+                        {
+                                temparray[i] = temparray[i] + 1.0;
+                                CountLettersEng++;
+                                
+                        }
+                        if(trueOrFalse >= 161 && trueOrFalse <= 252)
+                        {
+                                temparray[i] = temparray[i] + 1.0;
+                                CountLettersRu++;
+                                
+                        }
+                        /*else
+                        {
+                                temparray[i] = temparray[i] + 1.0;
+                                CountSymbols++;
+                        }*/
+                  
+                        temparray[i] = temparray[i] + 1.0;  
+            }
+        } 
+        temparray[i] = 100.0 * temparray[i] / array.Length;
+        if (temparray[i] > 0.0)
+        {
+            Console.WriteLine($"    {ascii[i]}    Symbol be found:    {tempCount:D3}    times =>    {temparray[i]:F2} %    of all characters");
+        }
+        tempCount = 0;
+    }
+    Console.WriteLine($"\n   There are   {CountLettersEng:D2}   english letters \n   There are   {CountLettersRu:D2}   cyrillic letters\n   There are   {CountNumbers:D2}   numbers \n   There are   {CountSymbols:D2}   symbols \n");
+    Console.WriteLine("\n");
+    return temparray;
+}
+
+string ascii =" !\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~"; //алфавит 32-127 ASCII   //массив - текст
+
+// Через ввод пользователя
+/*Console.WriteLine("\n Input your text: \n");
+string array = Console.ReadLine()!;
+char[] arrray = array.ToCharArray();*/
+
+// Через подгрузку текстового файла
+string array = System.IO.File.ReadAllText(@"D:\Разработчик\Введение-в-прогу\3_Esli-sdelayu-stanu-Senior'om-100proc\6_TwoDimensional-Arrays\task-60+\Text.txt");
+char[] arrray = array.ToCharArray();
+
+
+double [] darrayoutput = new double[ascii.Length];
+
+Console.WriteLine("\n Powers of characters:\n ");
+
+darrayoutput = PowerOfString(arrray, ascii);
--- 3_Esli-sdelayu-stanu-Senior'om-100proc/6_TwoDimensional-Arrays/task-60/task-60.cs
+++ 3_Esli-sdelayu-stanu-Senior'om-100proc/6_TwoDimensional-Arrays/task-60/task-60.cs
@@ -19,8 +19,10 @@ char[,] RandomArrayofChars(ref char[,] input)
 //печать массива
 void PrintRandomArrayofChars(char[,] input)
 {
+    
     for (int i = 0; i < input.GetLength(0); i++)
     {
+         Console.Write("  ");
         for (int j = 0; j < input.GetLength(1); j++)
         {
             Console.Write($"   {input[i,j]}");   
@@ -29,9 +31,17 @@ void PrintRandomArrayofChars(char[,] input)
     }
 }
 
+// метод для рандомного массива двумерного массива
 double[] PowerOfString (char[,] array, string ascii)
 {
     double[] temparray = new double[ascii.Length];
+    int CountSymbols = 0;
+    int CountNumbers = 0;
+    int CountLettersEng = 0;
+    int CountLettersRu = 0;
+    int tempCount = 0;
+    int trueOrFalse = 0;
+
     for (int i = 0; i < ascii.Length; i++)
     {
         for (int j = 0; j < array.GetLength(0); j++)
@@ -40,35 +50,62 @@ double[] PowerOfString (char[,] array, string ascii)
             {
                 if (array[j,k] == ascii[i])
                 {
+                        trueOrFalse = Convert.ToInt32(array[j,k]);
+                        if(trueOrFalse >= 48 && trueOrFalse <= 57)
+                        {
+                                temparray[i] = temparray[i] + 1.0;
+                                CountNumbers++;
+                                break;
+                        }
+                        if(trueOrFalse >= 65 && trueOrFalse <= 122)
+                        {
+                                temparray[i] = temparray[i] + 1.0;
+                                CountLettersEng++;
+                                break;
+                        }
+                        if(trueOrFalse >= 161 && trueOrFalse <= 252)
+                        {
+                                temparray[i] = temparray[i] + 1.0;
+                                CountLettersRu++;
+                                break;
+                        }
+                        else
+                        {
+                                temparray[i] = temparray[i] + 1.0;
+                                CountSymbols++;
+                        }
+                  
                     temparray[i] = temparray[i] + 1.0;
+                    tempCount++;
                 }
-            }   
+            }
         } 
         temparray[i] = 100.0 * temparray[i] / (array.GetLength(0) * array.GetLength(1));
+        
         if (temparray[i] > 0.0)
         {
-            Console.WriteLine($"   {ascii[i]}: {temparray[i]} %");
+            Console.WriteLine($"    {ascii[i]}    Symbol be found:    {tempCount}    times =>    {temparray[i]} %    of all characters");
         }
+        tempCount = 0;
     }
+    Console.WriteLine($"\n   There are   {CountLettersEng:D2}   english letters \n   There are   {CountLettersRu:D2}   cyrillic letters\n   There are   {CountNumbers:D2}   numbers \n   There are   {CountSymbols:D2}   symbols \n");
     Console.WriteLine("\n");
     return temparray;
 }
 
 string ascii =" !\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~"; //алфавит 32-127 ASCII
 
-char[,] darray = new char[4,4];    //массив - текст
+char[,] darray = new char[10,10];    //массив - текст
+
+char[] array = Console.ReadLine()!;
 double [] darrayoutput = new double[ascii.Length]; //массив частот
 
 darray = RandomArrayofChars(ref darray);
-Console.WriteLine("\n Сгенерирован следующий массив:\n");
+
+Console.WriteLine("\n Random array:\n");
+
 PrintRandomArrayofChars(darray);
-Console.WriteLine("\n Массив мощностей:\n ");
+
+Console.WriteLine("\n Powers of characters:\n ");
 
 darrayoutput = PowerOfString(darray, ascii);
-
-string kek = "";
-for (int  j= 0; j < 255; j++)
-{
-    kek = String.Concat(kek,Convert.ToChar(j));    
-}
-Console.WriteLine(kek);
