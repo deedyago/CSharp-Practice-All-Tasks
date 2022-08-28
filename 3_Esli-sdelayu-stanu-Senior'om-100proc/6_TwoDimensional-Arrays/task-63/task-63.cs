58c84fd5cf918b92629555c6e95128640a3f7ca0
deedyago <deedyagobin@gmail.com>
2022-08-28 23:18:18 +0300

    64/76 DONE

---
+++ 3_Esli-sdelayu-stanu-Senior'om-100proc/6_TwoDimensional-Arrays/task-63/task-63.cs
@@ -0,0 +1,57 @@
+﻿/// 63. Сформировать трехмерный массив не повторяющимися двузначными числами показать его построчно на экран выводя индексы соответствующего элемента
+
+
+//Ввод данных
+Console.WriteLine("Input depth of yr random array: ");
+int z = int.Parse(Console.ReadLine())!;
+
+Console.WriteLine("Input strokes of yr random array: ");
+int m = int.Parse(Console.ReadLine())!;
+
+Console.WriteLine("Input columns of yr random array: ");
+int n = int.Parse(Console.ReadLine())!;
+
+Console.WriteLine("Input range --from-- what number: ");
+int min = int.Parse(Console.ReadLine())!;
+
+Console.WriteLine("Input range --to-- what number: ");
+int max = int.Parse(Console.ReadLine())!;
+
+int[,,] array = new int[z, m, n];
+
+//Создание рандомного тройного массивы
+void RandomArray(int[,,] yoursarray)
+{
+    for (int i = 0; i < z; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            for(int k = 0; k < n; k++)
+            {
+                yoursarray[i, j, k] = new Random().Next(min, max);
+            }
+        }
+    }
+}
+RandomArray(array);
+
+Console.WriteLine();
+
+//Вывод рандомного массива с индексами элементов
+void RandomArrayPrint(int[,,] yoursarray)
+{
+    for (int i = 0; i < z; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            for (int k = 0; k < n; k++)
+            {
+                Console.Write($"{yoursarray[i, j, k]} - [{i},{j},{k}] ");
+                Console.WriteLine();
+            }
+            
+        }
+
+    }
+}
+RandomArrayPrint(array);
---
+++ 3_Esli-sdelayu-stanu-Senior'om-100proc/6_TwoDimensional-Arrays/task-64/task-64.cs
@@ -0,0 +1,60 @@
+﻿// 64. Показать треугольник Паскаля
+
+
+
+using _0_Library;
+
+//Console.WriteLine("Input depth of yr random array: ");
+//int z = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Input strokes of yr random array: ");
+int m = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Input columns of yr random array: ");
+int n = int.Parse(Console.ReadLine()!);
+
+
+
+
+
+
+int[,] RandomArray(int[,] array)
+{
+    int[,] darray = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            darray[i,j] = 1;
+        }
+    }
+    return darray;
+}
+
+int[,] darray = new int[m,n];
+darray = RandomArray(darray);
+
+int[,] Pascal(int[,] array)
+{
+    for (int i = 1; i < m; i++)
+    {
+        for (int j = 1; j < n; j++)
+        {
+            array[i,j] = array[i,j-1] + array[i-1,j];
+        }
+    }
+    return darray;
+}
+darray = Pascal(darray);
+
+for(int i = 0; i < darray.GetLength(0) ; i++)
+{
+    for (int j = 0;  j < darray.GetLength(1); j++)
+    {
+       // if(i )
+        Console.Write($" [{darray[i, j]:D2}]");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine("\n");
+
